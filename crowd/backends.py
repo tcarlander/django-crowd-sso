5b79403e235689aae84811139a376bd101d71a34
Tobias Carlander <tobias.carlander@wfp.org>
2015-04-08 21:56:27 +0400

    Replaced httplib2 with requests
    Switching from XML to JSon

--- crowd/backends.py
+++ crowd/backends.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from xml.dom.minidom import parseString
 from django.contrib.auth.backends import ModelBackend
-import httplib2
+import requests, json
+
 
 class CrowdBackend(ModelBackend):
     """
@@ -16,21 +17,20 @@ class CrowdBackend(ModelBackend):
         crowd_config = self._get_crowd_config()
         if not crowd_config:
             return None
+#        try:
         user = self._find_existing_user(username)
-        resp, content = self._call_crowd(username, password, crowd_config)
-        if resp['status'] == '200':
+        resp, content = self._call_crowd_session(username, password, crowd_config)
+        if resp == 201:
             if not user:
                 user = self._create_new_user_from_crowd_response(username, crowd_config)
             return user
         else:
             return None
-<<<<<<< HEAD
+ #       except:
+ #           return None
             
 
-=======
 
-        
->>>>>>> 65b7eaf... Now SSO works incoming
     def _get_crowd_config(self):
         """
         Returns CROWD-related project settings. Private service method.
@@ -50,67 +50,36 @@ class CrowdBackend(ModelBackend):
         else:
             return users[0]
 
-    def _call_crowd(self, username, password, crowd_config):
-        """
-        Calls CROWD webservice. Private service method.
-        """
-        body='<?xml version="1.0" encoding="UTF-8"?><password><value><![CDATA[%s]]></value></password>' % password
-        h = httplib2.Http()
-        h.add_credentials(crowd_config['app_name'], crowd_config['password'])
-        url = crowd_config['url'] + ('/usermanagement/latest/authentication?username=%s' % (username,))
-        resp, content = h.request(url, "POST", body=body, headers={'content-type': 'text/xml'})
-        return resp, content # sorry for this verbosity, but it gives a better understanding
 
 
     def _call_crowd_session(self, username, password, crowd_config):
         """
         Calls CROWD webservice. Private service method.
         """
-        body='<?xml version="1.0" encoding="UTF-8"?><authentication-context><username>%s</username> <password><![CDATA[%s]]></password>  <validation-factors>    <validation-factor><name>remote_address</name><value>127.0.0.1</value>    </validation-factor>  </validation-factors></authentication-context>' % (username,password)
-        h = httplib2.Http()
-        h.add_credentials(crowd_config['app_name'], crowd_config['password'])
-        url = crowd_config['url'] + ('/usermanagement/latest/session')
-        resp, content = h.request(url, "POST", body=body, headers={'content-type': 'text/xml'})
-        return resp, content # sorry for this verbosity, but it gives a better understanding
-
-
+        url = crowd_config['url'] + ('/usermanagement/latest/session.json') 
+        json_object = {'username' : username ,'password' : password ,'validation-factors' : {'validationFactors' : [{'name' : 'remote_address','value' : '127.0.0.1'}]}}
+        print( json_object)
+        r = requests.post(url, auth=(crowd_config['app_name'], crowd_config['password']),data=json.dumps(json_object),headers={'content-type': 'application/json','Accept': 'application/json'})
+        resp = r.status_code
+        print (resp)
+        print(r.json())
+        return resp, None # sorry for this verbosity, but it gives a better understanding
 
 
     def _create_new_user_from_crowd_response(self, username, crowd_config):
         """
         Creating a new user in django auth database basing on information provided by CROWD. Private service method.
         """
-        h = httplib2.Http()
-        h.add_credentials(crowd_config['app_name'], crowd_config['password'])
-        url = crowd_config['url'] + ('/usermanagement/latest/user?username=%s' % (username,))
-        resp, content = h.request(url, "GET")
-        content_parsed = CrowdBackend._parse_crowd_response(content)
+        
+        url = crowd_config['url'] + ('/usermanagement/latest/user.json?username=%s' % (username,))
+        r = requests.get(url, auth=(crowd_config['app_name'], crowd_config['password']))
+        content_parsed = r.json()
         user = User.objects.create_user(username, content_parsed['email'])
         user.set_unusable_password()
-        user.first_name = content_parsed['first_name']
-        user.last_name = content_parsed['last_name']
+        user.first_name = content_parsed['first-name']
+        user.last_name = content_parsed['last-name']
         user.is_active = True
         user.is_superuser = crowd_config.get('superuser', False)
         user.is_staff = crowd_config.get('staffuser', False)
         user.save()
         return user
-
-    def _parse_crowd_response( content):
-        """
-        Returns e-mail, first and last names of user from provided CROWD response. Private service method.
-        """
-        dom = parseString(content)
-        return {
-            'email': CrowdBackend._get_user_parameter_from_dom_tree(dom, 'email'),
-            'first_name': CrowdBackend._get_user_parameter_from_dom_tree(dom, 'first-name'),
-            'last_name': CrowdBackend._get_user_parameter_from_dom_tree(dom, 'last-name'),
-            'user': dom.getElementsByTagName('user')[0].attributes["name"].value            
-    
-        }
-
-    def _get_user_parameter_from_dom_tree(dom, parameter):
-        """
-        A small service method for dom tree parsing. Private service method.
-        """
-        # here I'm starting to doubt if a method that small is still a good refactoring practice. Still, no worries, eh?
-        return dom.getElementsByTagName(parameter)[0].firstChild.nodeValue
--- crowd/middleware.py
+++ crowd/middleware.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from xml.dom.minidom import parseString
 from django.contrib.auth import authenticate, login
-import httplib2
+import requests
 from crowd.backends import CrowdBackend
 
 class CookieMiddleware( object ):
@@ -14,7 +14,7 @@ class CookieMiddleware( object ):
         if "crowd.token_key" not in request.COOKIES:
             return
         token= request.COOKIES["crowd.token_key"]
-        username = self.get_the_user_from_token(token)
+        username = self.get_the_user_from_token_r(token)
         if username :
             try:
                 user = User.objects.filter(username=username)[0]
@@ -33,15 +33,17 @@ class CookieMiddleware( object ):
         login(request, None)
 
 
+<<<<<<< HEAD
 
     def get_the_user_from_token(self,token):
+=======
+    def get_the_user_from_token_r(self,token):
+>>>>>>> fa8e51c... Replaced httplib2 with requests
         crowd_config = CrowdBackend._get_crowd_config(self)
-        h = httplib2.Http()
-        h.add_credentials(crowd_config['app_name'], crowd_config['password'])
         url = crowd_config['url'] + ('/usermanagement/latest/session/%s'% (token,))
-        resp, content = h.request(url, "GET")
-        if resp.status < 400:
-            content_parsed = CrowdBackend._parse_crowd_response(content)
+        r = requests.get(url, auth=(crowd_config['app_name'], crowd_config['password']))
+        if r.status_code < 400:
+            content_parsed = CrowdBackend._parse_crowd_response(r.text)
         else:
             return None
         return content_parsed['user']
