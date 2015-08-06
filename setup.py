50e3e763c3631fb245f02f722ad0e0380d9d29a8
Tobias Carlander <tobias.carlander@wfp.org>
2015-08-06 10:36:58 +0400

    Trying without requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,8 @@ from distutils.core import setup
 
 setup(
     name='django-crowd',
-    version='0.45',
+    version='0.45.1',
     packages=['crowd', ],
-    install_requires=[
-        "requests",
-        "django",
-    ],
 
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
