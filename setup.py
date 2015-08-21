b8ffa2ef8f28077715b7ff1c0eeca1b5dd2fe442
Tobias Carlander <tobias.carlander@wfp.org>
2015-08-21 15:38:16 +0700

    Upped Verison

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-crowd',
-    version='0.45.1',
+    version='0.46',
     packages=['crowd', ],
 
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
