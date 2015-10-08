b0cd8a263623579c392f7cce7f355b0fb4a8abad
eddyzhow <grit.ni@gmail.com>
2015-10-08 16:22:47 +0700

    Increse the version number so feeder can get the update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-crowd',
-    version='0.46',
+    version='0.46.1',
     packages=['crowd', ],
     install_requires=['django','requests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
