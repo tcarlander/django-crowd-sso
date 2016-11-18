305ff08d1bf9beb425bda166d1bd96d59ed5b0cd
Tobias Carlander <tobias.carlander@wfp.org>
2016-11-18 17:03:45 +0700

    Fix Testing

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='wfp-django-crowd',
+    name='django-crowd-with-sso',
     version='0.56',
     description='Atlassina Crowd integration for Django',
     packages=['crowd', ],
-    install_requires=['django', 'requests', 'simplejson', 'pandoc'],
+    install_requires=['django', 'requests', 'simplejson'],
     url='https://github.com/WFP-BKK/django-crowd',
     author='Tobias Carlander',
     author_email='tobias.carlander@wfp.org',
@@ -24,7 +24,6 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     long_description=README,
-
     keywords='django crowd users',
 
 )
